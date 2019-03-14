92b23907cff15928f59cbc724084d0aae36590d4
Yukon <ywainczak@gmail.com>
2019-03-14 18:07:54 -0500

    Frank work.
     + Started to implement frank.

---
+++ Assets/Scripts/Controllers/BillboardController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BillboardController : MonoBehaviour
+{
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(transform.position + camera.transform.rotation * Vector3.forward, camera.transform.rotation * Vector3.up);
+    }
+}
