81aea40b5504e134d36e7942b7751864083e8774
Yukon <ywainczak@gmail.com>
2019-03-17 11:38:30 -0700

    Bleh

---
+++ Assets/Scripts/Controllers/VersionController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionController : MonoBehaviour
+{
+	[SerializeField]
+	private string versionPrefex;
+	
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	private void Awake()
+	{
+		gameObject.GetComponent<Text>().text = versionPrefex + Application.version;
+	}
+}
